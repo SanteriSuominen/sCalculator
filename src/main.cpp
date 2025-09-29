2599850fbae3871fcb7ccc9188653cad329ef3c7
SanteriSuominen <93214898+SanteriSuominen@users.noreply.github.com>
2025-09-29 10:06:30 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    // Test if your code Build&Runs from task runner press "Make" first!
+    int x, y;
+    x = 1;
+    y = 2;
+
+    std::cout << "x = " << x << ", y = " << y << ", x + y = " << (x + y) << std::endl;   
+    // You can also just build and run it form exmpl: ubuntu terminal just with "./"
+}
